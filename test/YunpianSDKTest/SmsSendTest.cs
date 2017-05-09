b78f2960a31916c18fd8ed66993940d8b06754df
刘金生 <ljs@qq.com>
2017-05-09 16:51:17 +0800

    测试

--- test/YunpianSDKTest/SmsSendTest.cs
+++ test/YunpianSDKTest/SmsSendTest.cs
@@ -17,6 +17,7 @@ namespace YunpianSDKTest
             var configBuilder = new ConfigurationBuilder();
             configBuilder.AddEnvironmentVariables();
             Configration = configBuilder.Build();
+            // 测试时请在测试机上设置该环境变量
             YunConfig = new Config(Configration["YunPian-AppKey"]);
         }
 
@@ -24,7 +25,7 @@ namespace YunpianSDKTest
         public void SingleTest()
         {
             var client = new SmsOperator(YunConfig);
-            var result = client.SingleSend("15194118449", "【钜学教育】您的验证码是：123456");
+            var result = client.SingleSend("15726102865", "【钜学教育】您的验证码是：123456");
             Assert.True(result.success);
         }
     }
